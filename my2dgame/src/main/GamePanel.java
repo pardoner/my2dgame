5ee926c9c412a8ab36228c16972722fbbc6ff3f1
Dylan Wicker <dylanwicker27@gmail.com>
2024-03-06 14:40:56 -0500

    switched to delta method

--- my2dgame/src/main/GamePanel.java
+++ my2dgame/src/main/GamePanel.java
@@ -19,13 +19,23 @@ public class GamePanel extends JPanel implements Runnable {
 	final int screenWidth = tileSize * maxScreenCol; // 768 pixels 
 	final int screenHeight = tileSize * maxScreenRow; // 576 pixels
 	
+	int FPS = 60;
+	
 	KeyHandler keyH = new KeyHandler();
 	Thread gameThread;
+	
+	// set players default postion
+	
+	int playerX = 100;
+	int playerY = 100;
+	int playerSpeed = 4;
 	
 	public GamePanel() {
 		this.setPreferredSize(new Dimension(screenWidth, screenHeight));
 		this.setBackground(Color.black);
 		this.setDoubleBuffered(true);
+		this.addKeyListener(keyH);
+		this.setFocusable(true);
 	}
 	
 	public void startGameThread() {
@@ -34,23 +44,96 @@ public class GamePanel extends JPanel implements Runnable {
 		
 	}
 	
+//	public void run() {
+//		
+//		double drawInterval = 1000000000/FPS;
+//		double nextDrawTime = System.nanoTime() + drawInterval;
+//		
+//		while (gameThread != null) {
+//			
+//			long currentTime = System.nanoTime();
+//			
+//	// 		System.out.println("The game loop is running");
+//			update();
+//			
+//			repaint();
+//			
+//			
+//			try {
+//				double remainingTime = nextDrawTime - System.nanoTime();
+//				remainingTime = remainingTime/1000000;
+//				
+//				if (remainingTime < 0) {
+//					remainingTime = 0;
+//				}
+//				
+//				Thread.sleep((long) remainingTime);
+//				
+//				nextDrawTime += drawInterval;
+//				
+//			} catch (InterruptedException e) {
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//			}
+//		}
+//	}
+	
 	public void run() {
+		double drawInterval = 1000000000/FPS;
+		double delta = 0;
+		long lastTime = System.nanoTime();
+		long currentTime;
+		long timer = 0;
+		int drawCount = 0;
+		
 		while (gameThread != null) {
-	// 		System.out.println("The game loop is running");
-			update();
 			
-			repaint();
+			currentTime = System.nanoTime();
+			
+			delta += (currentTime - lastTime) / drawInterval;
+			timer += (currentTime - lastTime);
+			
+			lastTime = currentTime;
+			
+			if (delta >= 1) {
+				update();
+				repaint();
+				delta--;
+				drawCount++;
+			}
+			
+			if (timer >= 1000000000) {
+				System.out.println("FPS:" + drawCount);
+				drawCount = 0;
+				timer = 0;
+				
+			}
 		}
 	}
+	
+	
 	public void update() {
+		if (keyH.upPressed == true) {
+			playerY -= playerSpeed;
+		} 
+		else if (keyH.downPressed == true) {
+			playerY += playerSpeed;
+		}
+		else if (keyH.leftPressed == true) {
+			playerX -= playerSpeed;
+		}
+		else if (keyH.rightPressed == true) {
+			playerX += playerSpeed;
+		}
 		
 	}
+	
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		Graphics2D g2 = (Graphics2D)g;
 		
 		g2.setColor(Color.white);
-		g2.fillRect(100, 100, tileSize, tileSize);
+		g2.fillRect(playerX, playerY, tileSize, tileSize);
 		g2.dispose();
 	}
 }
--- my2dgame/src/main/KeyHandler.java
+++ my2dgame/src/main/KeyHandler.java
@@ -33,7 +33,20 @@ public class KeyHandler implements KeyListener{
 
 	@Override
 	public void keyReleased(KeyEvent e) {
-		// TODO Auto-generated method stub
+		int code = e.getKeyCode();
+		
+		if (code == KeyEvent.VK_W) {
+			upPressed = false;
+		}
+		if (code == KeyEvent.VK_S) {
+			downPressed=false;
+		}	
+		if (code == KeyEvent.VK_A) {
+			leftPressed=false;
+		}	
+		if (code == KeyEvent.VK_D) {
+			rightPressed=false;
+		}	
 		
 	}
 
