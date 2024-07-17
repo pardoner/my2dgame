231a312e6c1933e6fcd15395fdbbbebe5dbaef3c
Dylan Wicker <dylanwicker27@gmail.com>
2024-07-17 16:31:26 -0400

    centered player to screen

--- my2dgame/src/entity/Player.java
+++ my2dgame/src/entity/Player.java
@@ -12,10 +12,16 @@ import main.KeyHandler;
 public class Player extends Entity {
 	GamePanel gp;
 	KeyHandler keyH;
+	
+	public final int screenX;
+	public final int screenY;
 
 	public Player(GamePanel gp, KeyHandler keyH) {
 		this.gp = gp;
 		this.keyH = keyH;
+		
+		screenX = gp.screenWidth/2;
+		screenY = gp.screenHeight/2;
 
 		setDefaultValues();
 		getPlayerImage();
@@ -23,8 +29,8 @@ public class Player extends Entity {
 	}
 
 	public void setDefaultValues() {
-		x = 100;
-		y = 100;
+		worldX = gp.tileSize * 23;
+		worldY = gp.tileSize * 21;
 		speed = 4;
 		direction = "down";
 	}
@@ -53,19 +59,19 @@ public class Player extends Entity {
 
 			if (keyH.upPressed) {
 				direction = "up";
-				y -= speed;
+				worldY -= speed;
 			}
 			else if (keyH.downPressed) {
 				direction = "down";
-				y += speed;
+				worldY += speed;
 			}
 			else if (keyH.leftPressed) {
 				direction = "left";
-				x -= speed;
+				worldX -= speed;
 			}
 			else if (keyH.rightPressed) {
 				direction = "right";
-				x += speed;
+				worldX += speed;
 			}
 
 			spriteCounter++;
