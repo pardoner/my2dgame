2086a72995a4ee7e4e7ac2b4a67cc0ba1a1d44be
Dylan Wicker <dylanwicker27@gmail.com>
2024-08-06 17:55:54 -0400

    added messages

--- my2dgame/src/entity/Player.java
+++ my2dgame/src/entity/Player.java
@@ -16,7 +16,7 @@ public class Player extends Entity {
 	
 	public final int screenX;
 	public final int screenY;
-	int hasKey = 0;
+	public int hasKey = 0;
 
 	public Player(GamePanel gp, KeyHandler keyH) {
 		this.gp = gp;
@@ -128,19 +128,30 @@ public class Player extends Entity {
 				gp.playSE(1);
 				hasKey++;
 				gp.obj[i]=null;
+				gp.ui.showMessage("You got a key!");
 				break;
 			case "Door":
 				if (hasKey > 0) {
 					gp.playSE(3);
 					gp.obj[i] = null;
 					hasKey--;
+					gp.ui.showMessage("You opened the door!");
+				}
+				else {
+					gp.ui.showMessage("You need a key!");
 				}
 				break;
 			case "Boots": 
 				gp.playSE(2);
 				speed+=2;
 				gp.obj[i] = null;
+				gp.ui.showMessage("Speed up!");
 				break;
+			case "Chest": 
+				gp.ui.gameFinished = true;
+				gp.stopMusic();
+				gp.playSE(4);
+				break;	
 			}
 		}
 	}
